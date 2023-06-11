b1ba57684a181c5f09b2f0de31a46cbe1499bfac
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-06-10 22:14:16 -0500

    Sección 10

--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -6,27 +6,25 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
-
-
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
     AboutPageComponent,
-    HomePageComponent,
-    SidebarComponent,
     ContactPageComponent,
-    SearchBoxComponent
+    HomePageComponent,
+    LoadingSpinnerComponent,
+    SearchBoxComponent,
+    SidebarComponent,
   ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
+  imports: [CommonModule, RouterModule],
   exports: [
     AboutPageComponent,
-    HomePageComponent,
-    SidebarComponent,
     ContactPageComponent,
+    HomePageComponent,
+    LoadingSpinnerComponent,
     SearchBoxComponent,
-  ]
+    SidebarComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
