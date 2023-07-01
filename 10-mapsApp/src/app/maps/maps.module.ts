4cdecc246c181890a552475d5a9cde0e1fe19c25
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-06-30 19:43:46 -0500

    Sección 20

--- 10-mapsApp/src/app/maps/maps.module.ts
+++ 10-mapsApp/src/app/maps/maps.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+(mapboxgl as any).accessToken =
+  'pk.eyJ1IjoiYW50aG9ueS1hdiIsImEiOiJjbGpqN2psb2EwNmdnM3BxOXg3YTBkM2RkIn0.3H1Xne01RB2zWwwdIjc-zQ';
+
+
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
